5b05a01b0c6ae2ea04a78dd0de54172bc47d2656
Menua <menua.hovhannisyan@student.kdg.be>
2023-02-05 21:07:51 +0100

    some minor changes

--- tailwind.js
+++ tailwind.js
@@ -5,7 +5,8 @@ module.exports = {
         'mfc-dark-gray': '#272727',
         'mfc-light-gray': '#cecece',
         'mfc-yellow': '#ffff3d',
-        'mfc-dark-light-gray': '#929292'
+        'mfc-dark-light-gray': '#929292',
+        'mfc-light-blue': '#68B8D9'
       },
     }
   }
